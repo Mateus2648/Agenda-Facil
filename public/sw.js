98ede0bbac40f884978af4db3aeab1b674107135
Mateus Ferreira <mateusaparecidoferreira@outlook.com>
2026-04-03 09:48:14 -0300

    Progressive Web App

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'agenda-facil-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/favicon.svg',
+  '/manifest.json'
+];
+
+// Install event - caching basic assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate event - cleaning up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event - network-first strategy for basic offline support
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
